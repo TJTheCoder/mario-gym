b01d3389ff12a3623f8ad5f1019ecfd25a4b664c
TJTheCoder <49538138+TJTheCoder@users.noreply.github.com>
2025-05-08 16:28:36 -0500

    Update setup.py

--- gym_retro/setup.py
+++ gym_retro/setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="gym_retro",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "gymnasium>=0.29.1,<1.1.0",
+        "numpy"
+    ],
+)
